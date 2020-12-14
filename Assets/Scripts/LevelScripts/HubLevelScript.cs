5dab1ee1e80c019754584379c8fd65e796af3f8b
Steven Rachman <steven.rachman@gmail.com>
2020-12-15 01:24:57 +0800

    Fix to remove listeners when loading Hub Scene

---
+++ Assets/Scripts/LevelScripts/HubLevelScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubLevelScript : LevelScript
+{
+    public override void OnSceneLoad()
+    {
+        var inventory = Store.Instance.player.inventory;
+        inventory.ResetListeners();
+    }
+}
